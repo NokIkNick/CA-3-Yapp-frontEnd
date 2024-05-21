c2fd50e5957e5b58757b3df32de03c056d4a89a2
Nicklas W. <Nicklas2002@hotmail.com>
2024-05-21 12:23:43 +0200

    begun styling the login

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -5,7 +5,52 @@ const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    height: 100vh;
+    form{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        input{
+            padding: 0.5rem;
+            border-radius: 5px;
+            border: none;
+        }
+        button{
+            padding: 0.5rem;
+            border-radius: 5px;
+            border: none;
+            background-color: var(--dark-green);
+            color: var(--offwhite);
+            cursor: pointer;
+        }
+    }
 `
+
+const Footer = styled.div`
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    gap: 1rem;  
+    button{
+        background-color: transparent;
+        border: none;
+        color: var(--offwhite);
+        cursor: pointer;
+    }
+    background-color: var(--dark-green);
+    div{
+        display: flex;
+        gap: 1rem;
+        margin-top: 1rem; 
+        margin-bottom: 1rem;
+    }
+    p{
+        color: var(--offwhite);
+    }
+`;
 
 
 export const Login = () => {
@@ -18,11 +63,13 @@ export const Login = () => {
                 <button type="submit">Login</button>
                 <button type="button">Forgot password?</button>
             </form>
+        </Container>
+        <Footer>
             <div>
                 <p>Don't have an account?</p>
                 <button type="button">Sign up here</button>
             </div>
-        </Container>
+        </Footer>
     </>
   )
 }
