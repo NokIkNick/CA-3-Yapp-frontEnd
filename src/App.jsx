ccc50e51bc227b2f4903a8f3e8b5f5ee37a23e71
Nicklas W. <Nicklas2002@hotmail.com>
2024-05-22 12:07:47 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,6 @@ function App() {
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<Register />}/>
           <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Login />}/>
-          <Route path="/register" element={<Register />}/>
         </Routes>
       </BrowserRouter>
     </div>
