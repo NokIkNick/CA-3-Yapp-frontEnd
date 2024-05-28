662ee7c08fd7e581aee771f974360175387cf9d1
Nicklas W. <Nicklas2002@hotmail.com>
2024-05-28 16:17:25 +0200

    Little merged conflict fix

--- src/services/apiFacade.js
+++ src/services/apiFacade.js
@@ -73,6 +73,29 @@ export const fetchCategories = async () => {
     }
     return data;
 }
+
+export const createThread = async (thread, token) => {
+    const response = await fetch(`${BASE_URL}/protected/createThread`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+    },
+        body: JSON.stringify({
+            title: thread.title,
+            content: thread.content,
+            category: thread.category,
+            userName: thread.author
+        })
+    });
+    const data = await response.json();
+    if(!data){
+        throw new Error("Could not create thread");
+    }
+    return data;
+}
+
+
 export function formatDate(createdDate) {
     // Extracting individual components
     const [year, month, day, hours, minutes, seconds, milliseconds] = createdDate;
